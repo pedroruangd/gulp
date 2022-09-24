ac620da355c60346da8737e4b40f63a8a2168fdd
Pedro Ruan <pedroruangd@gmail.com>
2022-09-23 22:00:46 -0300

    atualizacao de links js, css e imagens

--- gulpfile.js
+++ gulpfile.js
@@ -3,36 +3,44 @@ const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
-const image = require('gulp-image')
 
 function tarefasCSS(cb) {
 
-    return gulp.src('./vendor/**/*.css')
+    return gulp.src([
+            './vendor/**/*.css',
+            './css/*.css'
+        ])
         .pipe(concat('libs.css'))
         .pipe(cssmin())
-        .pipe(rename({ suffix: '.min'})) // libs.min.css
+        .pipe(rename({ suffix: '.min' })) // libs.min.css
         .pipe(gulp.dest('./dist/css'))
 
 }
 
-function tarefasJS(){
+function tarefasJS() {
 
-    return gulp.src('./vendor/**/*.js')
-        .pipe(concat('libs.js'))
+    return gulp.src([
+            './node_modules/jquery/dist/jquery.js',
+            './node_modules/bootstrap/dist/js/bootstrap.js',
+            './vendor/owl/js/owl.js',
+            './vendor/jquery-mask/jquery.mask.js',
+            './vendor/jquery-ui/jquery-ui.js'
+        ])
+        .pipe(concat('scripts.js'))
         .pipe(uglify())
-        .pipe(rename({ suffix: '.min'})) //libs.min.js
+        .pipe(rename({ suffix: '.min' })) //libs.min.js
         .pipe(gulp.dest('./dist/js'))
 }
 
 
-function tarefasImagem(){
-    
-    return gulp.src('./src/images/*')
+function tarefasImagem() {
+
+    return gulp.src('./assets/images/*')
         .pipe(image({
             pngquant: true,
-            optipng: false,
+            optipng: true,
             zopflipng: true,
-            jpegRecompress: false,
+            jpegRecompress: true,
             mozjpeg: true,
             gifsicle: true,
             svgo: true,
